39f3093a6c854917e781df7c7c8d59255096a829
Jan Trejbal <jan@trejbal.land>
2025-04-07 23:31:35 +0200

    Add missing usings

--- src/Demo/SafeRouting.Demo/Controllers/UsageExampleController.cs
+++ src/Demo/SafeRouting.Demo/Controllers/UsageExampleController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
 
 namespace SafeRouting.Demo.Controllers;
 
